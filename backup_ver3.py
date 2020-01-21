fcf938d4a1f8db6592dc24b292551fba9adfea63
Part1nax777 <serewka-ru@yandex.ru>
2020-01-21 21:47:10 +0300

    add comment to file name

--- backup_ver3.py
+++ backup_ver3.py
@@ -1,24 +1,24 @@
 import os
 import time
 
-source = ["/home/serewka/node_modules", "/home/serewka/Документы"]
+source = ["/home/serewka/Документы", "/home/serewka/Изображения"]
 target_dir = "/home/serewka"
 
 today = target_dir + os.sep + time.strftime('%Y%m%d')
 now = time.strftime('%H%M%S')
+comment = input('Введите комментарий ->> ')
+
+if len(comment) == 0:
+  target = today + os.sep + now + '.zip'
+else:
+  target = today + os.sep + now + '_' + comment.replace(' ', '_') + '.zip'
 
 if not os.path.exists(today):
   os.mkdir(today)
   print('Каталог успешно создан', today)
-target = today + os.sep + now + '.zip'
-print(target)
-print(source)
 zip_command = "zip -qr {0} {1}".format(target, ' '.join(source))
 
 if os.system(zip_command) == 0:
   print('Резервная копия успешно создана', target)
 else:
-  print('Создание резервной копии не удалось!')
-
-
-
+  print('Создание резервной копии не удалось')
